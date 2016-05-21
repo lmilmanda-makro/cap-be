733bdcc490655241b891e157d7746597f755f22d
javier Etchepare <elchepare@gmail.com>
2016-05-21 20:56:42 -0300

    Add files via upload

---
+++ application/config/nexmo.php
@@ -0,0 +1,8 @@
+<?php  (! defined('BASEPATH')) and exit('No direct script access allowed');
+
+// please get the following api on Nexmo Dashboard.
+$config['api_key'] = 'd19c0efa';
+$config['api_secret'] = '9bd698d939f6ad8d';
+
+/* End of file nexmo.php */
+/* Location: ./application/config/nexmo.php */
