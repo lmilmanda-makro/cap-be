138b5a120db7802c1ea7914cf96d7fd1524e3500
javier Etchepare <elchepare@gmail.com>
2016-05-23 16:22:51 -0300

    Add files via upload

--- application/config/nexmo.php
+++ application/config/nexmo.php
@@ -3,6 +3,11 @@
 // please get the following api on Nexmo Dashboard.
 $config['api_key'] = 'd19c0efa';
 $config['api_secret'] = '9bd698d939f6ad8d';
+$config['api_brand'] = 'Makro';
+$config['api_sender_id'] = 'VERIFY'; //optional
+$config['api_code_length'] = '4'; //optional 4 or 6
+$config['api_lg'] = 'es-es'; //optional nexmo default en-us https://docs.nexmo.com/index.php/voice-api/text-to-speech#languages
+$config['api_request_id'] = ''; //request id
 
 /* End of file nexmo.php */
 /* Location: ./application/config/nexmo.php */
