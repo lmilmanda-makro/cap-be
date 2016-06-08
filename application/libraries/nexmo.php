3090d3f5eff165af13878a64af96290de75c95f8
javier Etchepare <elchepare@gmail.com>
2016-06-08 17:24:07 -0300

    Add files via upload

--- application/libraries/nexmo.php
+++ application/libraries/nexmo.php
@@ -597,6 +597,8 @@ class nexmo
 
 	$ch = curl_init();
 	
+	echo $this->url;
+	
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_URL,$this->url);
